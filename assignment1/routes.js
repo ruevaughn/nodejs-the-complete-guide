6e700ea34201107842d2139ccad8f0c8e7f0d1d9
Chase Jensen <soccerchase@gmail.com>
2019-03-24 22:45:57 -0600

    Assignment 1 complete

--- assignment1/routes.js
+++ assignment1/routes.js
@@ -9,12 +9,16 @@ const routes = (req, res) => {
     res.write("  <head><title>assignment 1</title></head>")
     res.write("  <body>");
     res.write("    <h1>welcome to assignment 1! enjoy your stay.</h1>")
+    res.write("    <h3>Create a new User?</h3>");
+    res.write("    <form action='/create-user' method='POST'>");
+    res.write("      <input type='text' name='username'></input>");
+    res.write("      <button type='submit' value='submit'>New User</button>");
+    res.write("    </form>");
     res.write("  </body>");
     res.write("</html>");
     return res.end();
   }
   if (url === '/users') {
-    res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("  <body>");
     res.write("    <h3>Users</h3>");
@@ -26,6 +30,21 @@ const routes = (req, res) => {
     res.write("    </ul>");
     res.write("  </body>");
     res.write("</html>");
+    res.end();
+  }
+  if (url === '/create-user' && method == 'POST') {
+    const body = [];
+    req.on('data', chunk => {
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      console.log(message);
+      return res.end();
+    })
+    res.statusCode = 302; 
+    res.setHeader('Location', '/');
     res.end();
   }
 }
