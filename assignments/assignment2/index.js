30021b5eb75f3d0ee3ce87c584920d2c3868aa56
Chase Jensen <soccerchase@gmail.com>
2019-03-26 08:17:44 -0600

    Assignment 2 Part 3: Handle middleware requests to '/' and '/users'

--- assignments/assignment2/index.js
+++ assignments/assignment2/index.js
@@ -2,14 +2,12 @@ const express = require("express");
 const PORT = 3002;
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("First request matching all routes");
-  res.write("<h1>Header</h1>");
-  next();
+app.use("/users", (req, res, next) => {
+  res.write("<h1>Users Page</h1>");
 });
 
-app.use("/add-user", (req, res, next) => {
-  res.write("<h1>Add User Page</h1>");
+app.use("/", (req, res, next) => {
+  res.write("<h1>Homepage</h1>");
 });
 
 app.listen(PORT);
