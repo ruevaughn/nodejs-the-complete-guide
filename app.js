bc0e7ab7ecc521ba8a52131a72578686d6ea7547
Chase Jensen <soccerchase@gmail.com>
2019-03-26 07:32:06 -0600

    Use Express and middleware

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const http = require("http");
-const routes = require("./routes");
+const express = require("express");
+const app = express();
 const PORT = 3001;
-const server = http.createServer(routes);
 
-console.log(`Starting server on Port ${PORT}`);
+// Middleware, always receives (req, res, next). res is sent back with new tricks learned. next is called to proceed
+app.use((req, res, next) => {
+  console.log("In the Middleware!");
+  next();
+});
 
+app.use((req, res, next) => {
+  console.log("In another Middleware!");
+});
+
+const server = http.createServer(app);
 server.listen(PORT);
