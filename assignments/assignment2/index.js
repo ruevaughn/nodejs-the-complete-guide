372224ac19f61407fabc5f852fd7ba39cfd0ea58
Chase Jensen <soccerchase@gmail.com>
2019-03-26 08:15:56 -0600

    Assignment 2 Part 2-2: Change Middleware so it funnels through both requests

--- assignments/assignment2/index.js
+++ assignments/assignment2/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const PORT = 3002;
 const app = express();
 
-app.use("/add-user", (req, res, next) => {
-  res.write("<h1>Add User Page</h1>");
+app.use("/", (req, res, next) => {
+  console.log("First request matching all routes");
+  res.write("<h1>Header</h1>");
+  next();
 });
 
-app.use("/", (req, res, next) => {
-  res.write("<h1>HomePage</h1>");
+app.use("/add-user", (req, res, next) => {
+  res.write("<h1>Add User Page</h1>");
 });
 
 app.listen(PORT);
