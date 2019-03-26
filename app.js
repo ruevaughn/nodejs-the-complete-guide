65fcb57f1c9f223a1a16e60f79c01b6d78480461
Chase Jensen <soccerchase@gmail.com>
2019-03-25 19:37:37 -0600

    Add console log to display port listening on

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const http = require('http');
-const routes = require('./routes');
-
+const http = require("http");
+const routes = require("./routes");
+const PORT = 3001;
 const server = http.createServer(routes);
 
-server.listen(3001);
+console.log(`Starting server on Port ${PORT}`);
+
+server.listen(PORT);
