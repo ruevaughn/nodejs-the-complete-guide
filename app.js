7f3a6116a2d3b87b73398264ba3d17164714f2eb
Chase Jensen <soccerchase@gmail.com>
2019-03-27 10:08:10 -0600

    Modularize routes

--- app.js
+++ app.js
@@ -1,30 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
+
+// Routes
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+// Runtime Variables
 const PORT = 3001;
+const app = express();
 
 // Middleware, always receives (req, res, next). res is sent back with new tricks learned. next is called to proceed
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// app.use("/", (req, res, next) => {
-//   console.log(`${Date.now("%d")} - ${req.url}`);
-//   next();
-// });
-
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></input></form>"
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(PORT);
 console.log(`Listening on port: ${PORT}`);
