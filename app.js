84aea0353fd15706b0c24e20c7f6a0a908252088
Chase Jensen <soccerchase@gmail.com>
2019-03-27 08:36:57 -0600

    Use body-parser to parse incoming URL data and make it available on the request object

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3001;
 
 // Middleware, always receives (req, res, next). res is sent back with new tricks learned. next is called to proceed
-app.use("/", (req, res, next) => {
-  console.log(`${Date.now("%d")} - ${req.url}`);
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use("/", (req, res, next) => {
+//   console.log(`${Date.now("%d")} - ${req.url}`);
+//   next();
+// });
 
 app.use("/add-product", (req, res, next) => {
-  res.send("<h1>This is a page to add a Product!</h1>");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></input></form>"
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
