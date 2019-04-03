60841594749d00403abc5185e87dec5a0d765fb9
Chase Jensen <soccerchase@gmail.com>
2019-04-03 12:21:51 -0600

    Add Routing admin namespace

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ const app = express();
 
 // Middleware, always receives (req, res, next). res is sent back with new tricks learned. next is called to proceed
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(adminRoutes);
-app.use(shopRoutes);
+app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(PORT);
 console.log(`Listening on port: ${PORT}`);
