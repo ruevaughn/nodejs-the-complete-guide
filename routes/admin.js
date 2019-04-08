78abf26bfc47e7fef36b4f68e193fd22ffc7d700
Chase Jensen <soccerchase@gmail.com>
2019-04-07 23:02:45 -0600

    Add logic to serve files

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,9 @@
+const path = require('path');
 const express = require("express");
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/add-product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></input></form>"
-  );
+  res.sendfile(path.join(__dirname, "../views/add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const express = require("express");
 const router = express.Router();
 
 router.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
+  res.sendfile(path.join(__dirname, "../views/shop.html"));
 });
 
 module.exports = router;
