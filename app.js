7b78762b5dc1c3ff386545b0e2080f0faed77c20
Chase Jensen <soccerchase@gmail.com>
2019-04-07 23:29:04 -0600

    Add utility path helper

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
+
+const express = require("express");
+
+const rootDir = require("./util/path");
+// const bodyParser = require("body-parser");
 
 // Routes
 const adminRoutes = require("./routes/admin");
@@ -11,12 +14,12 @@ const PORT = 3001;
 const app = express();
 
 // Middleware, always receives (req, res, next). res is sent back with new tricks learned. next is called to proceed
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendfile(path.join(__dirname, "./views/404.html"));
+  res.status(404).sendfile(path.join(rootDir, "views", "404.html"));
 });
 
 app.listen(PORT);
