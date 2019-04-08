7e86c573de5a9b8fdde3b6fe844c06da5661eb98
Chase Jensen <soccerchase@gmail.com>
2019-04-07 23:15:23 -0600

    Update shop route and add 404

--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require("express");
 const router = express.Router();
 
-router.use("/", (req, res, next) => {
+router.use("/shop", (req, res, next) => {
   res.sendfile(path.join(__dirname, "../views/shop.html"));
 });
 
