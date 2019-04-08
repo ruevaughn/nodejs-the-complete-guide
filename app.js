d5ad5da4d4753efbc5d79ef845b988e9641dff16
Chase Jensen <soccerchase@gmail.com>
2019-04-07 23:02:01 -0600

    Re-add shop routes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware, always receives (req, res, next). res is sent back with new tricks learned. next is called to proceed
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/admin", adminRoutes);
-// app.use(shopRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
   res.status(404).send("<h1>Page not found</h1>");
