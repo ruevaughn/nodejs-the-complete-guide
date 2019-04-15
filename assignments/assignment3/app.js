82558d6f20ddb423e845c7db1ed2256c9a4c1e2a
Chase Jensen <soccerchase@gmail.com>
2019-04-15 09:49:34 -0600

    Assignment 3 complete

--- assignments/assignment3/app.js
+++ assignments/assignment3/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const app = express();
+const path = require("path");
 
 const pageRoutes = require("./routes/users");
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(pageRoutes);
 
 app.listen(3003);
