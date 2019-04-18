3a2a8d3711f95146a54eff1beeab0b39a1213707
Chase Jensen <soccerchase@gmail.com>
2019-04-17 23:28:28 -0600

    404 Page to pug

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendfile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render('404');
 });
 
 app.listen(PORT);
