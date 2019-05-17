b8896b2c1d8f5dfc189803670b21fa7a7510c9b8
Chase Jensen <soccerchase@gmail.com>
2019-05-16 18:01:50 -0600

    Remove add product view, change to edit product view, add get edit product route

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,14 +1,13 @@
 const Product = require("../models/product");
 
-exports.getAddProducts = (req, res, next) => {
-  res.render("admin/add-product", {
+exports.getAddProduct = (req, res, next) => {
+  res.render("admin/edit-product", {
     pageTitle: "Add Product",
-    path: "/admin/add-product",
-    activeAddProduct: true
+    path: "/admin/add-product"
   });
 };
 
-exports.postAddProducts = (req, res, next) => {
+exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const description = req.body.description;
@@ -16,6 +15,18 @@ exports.postAddProducts = (req, res, next) => {
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  res.render("admin/edit-product", {
+    pageTitle: "Edit Product",
+    path: "/admin/edit-product",
+    editing: editMode
+  });
 };
 
 exports.getProducts = (req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -3,9 +3,10 @@ const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admin");
 
-router.get("/add-product", adminController.getAddProducts);
+router.get("/add-product", adminController.getAddProduct);
+router.post("/add-product", adminController.postAddProduct);
 
-router.post("/add-product", adminController.postAddProducts);
+router.get("/edit-product/:productId", adminController.getEditProduct);
 
 router.get("/products", adminController.getProducts);
 
