571a0906a6eacf7d2145b9dd81d0663d39955d18
Chase Jensen <soccerchase@gmail.com>
2019-05-16 18:27:06 -0600

    Add edit view and route

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,8 @@ const Product = require("../models/product");
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
-    path: "/admin/add-product"
+    path: "/admin/add-product",
+    editing: false
   });
 };
 
@@ -22,10 +23,18 @@ exports.getEditProduct = (req, res, next) => {
   if (!editMode) {
     return res.redirect("/");
   }
-  res.render("admin/edit-product", {
-    pageTitle: "Edit Product",
-    path: "/admin/edit-product",
-    editing: editMode
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) {
+      console.log("No Product Found for Edit Product");
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product
+    });
   });
 };
 
